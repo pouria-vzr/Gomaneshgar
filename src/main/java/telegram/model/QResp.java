bed282be483276f3dd10824fc01e365297d84de2
pouria <pouria.vzr@gmail.com>
2017-02-25 21:02:01 +0330

    data structure has been added

--- src/main/java/telegram/model/QResp.java
+++ src/main/java/telegram/model/QResp.java
@@ -4,16 +4,48 @@ package telegram.model;
  * Created by pouria on 2/10/17.
  */
 public class QResp {
-    public int n;
-    public String asker;
-    public String question;
-    public String answer;
+    private int n;
+    private String asker;
+    private String question;
+    private String answer;
 
     public QResp() {
         n = -1;
         asker = "";
         question = "";
         answer = "";
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public String getAsker() {
+        return asker;
+    }
+
+    public void setAsker(String asker) {
+        this.asker = asker;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
     }
 
     @Override
